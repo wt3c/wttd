332cbec1a662946181fb804a360cd4768217bce5
Welington Carlos <wcarlos3@gmail.com>
2021-11-16 19:13:31 -0300

    O resgate do codigo não testado

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+"""First tests"""
+class HomeTest(TestCase):
+    """Home Tests"""
+    def setUp(self):
+        self.response = self.client.get('/')
 
-# Create your tests here.
+    def test_get(self):
+        """GET / must return status code 200"""
+        self.assertEqual(200, self.response.status_code)
+
+    def test_template(self):
+        """Must use index.html"""
+        self.assertTemplateUsed(self.response, 'index.html')
